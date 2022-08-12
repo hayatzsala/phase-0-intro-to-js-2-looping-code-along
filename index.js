2d2e972ab92e1e89724cb6db5da24f7420bf3eee
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-12 22:25:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names , thing){
+    let i =0;
+    const arr = [];
+    while (i< names.length){
+        var str = `Thank you, ${names[i]}, for the wonderful ${thing} gift!`;
+        arr.push(str);
+        i++;
+    }
+    return arr;
+}
+
+function countDown(num){
+    
+    for(let i =num; i>= 0;i--){
+       console.log(i);
+    }
+}
+
+countDown(4);
